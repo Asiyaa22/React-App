0f8603e04c4d15cf28b77b85a18ab11683ba85f4
Asiya <asiyacode@gmail.com>
2024-12-14 16:18:27 +0530

    Learning

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -3,13 +3,18 @@ import PropTypes from "prop-types";
 
 function Input(props){
     return <div>
-        <input type={props.type} placeholder={props.placeholder} />
+        <input type={props.type}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        value={props.value}/>
     </div>
 }
 
 Input.propTypes = {
     type: PropTypes.string.isRequired,
-    placeholder: PropTypes.string
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    value: PropTypes.string
 };
 
 export default Input;
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import Input from "./Input";
+// import Input from "./Input";
 
 function Register(){
     
     const [name, setName] = useState("")
 
-    function handleSubmit(event){
-        // setName(event.input.value)
-        console.log(event.target.value)
+    function handleChange(event){
+        setName(event.target.value);
+        console.log("Typing....", event.target.value);
     }
 
     return <div className="form-body">
@@ -15,7 +15,8 @@ function Register(){
             <p>Register Yourself</p>
             <h2>Hello {name}</h2>
             <form action="">
-              <Input type="text" onChange={handleSubmit} placeholder="UserName" value={name}/>
+              <input type="text" placeholder="UserName" onChange={handleChange} value={name}/>
+              {/* <Input type="text" onChange={handleChange} placeholder="UserName" value={name}/> */}
               {/* <Input type="password" placeholder="password" />
               <Input type="password" placeholder="Confrim Password" /> */}
               <button className="btn" type="submit">Register</button>
